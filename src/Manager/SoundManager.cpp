cd0a9ea4806448acacbede8e83354c87eb569af5
Mingyverse <teoh.yongming456@gmail.com>
2025-09-05 03:59:28 +0800

    sound manager class

---
+++ src/Manager/SoundManager.cpp
@@ -0,0 +1,28 @@
+#include "SoundManager.h"
+
+void SoundManager::InitializeAudio() {
+    result = FMOD::System_Create(&system);
+    result = system->init(32, FMOD_INIT_NORMAL, extradriverdata);
+}
+
+void SoundManager::PlaySound1(float pitch, float pan) {
+    result = system->playSound(sound1, 0, false, &channel);
+    channel->setPitch(pitch);
+    channel->setPan(pan);
+}
+
+void SoundManager::PlaySoundTrack() {
+    result = system->playSound(sound2, 0, false, &channel);
+}
+
+void SoundManager::LoadSounds() {
+    result = system->createSound("assets/128294__xenonn__layered-gunshot-2.wav", FMOD_DEFAULT, 0, &sound1);
+    result = sound1->setMode(FMOD_LOOP_OFF);
+
+    result = system->createStream("assets/659278__seth_makes_sounds__lofi-guitar-beat-70bpm.wav", FMOD_DEFAULT, 0, &sound2);
+    result = sound2->setMode(FMOD_LOOP_OFF);
+}
+
+void SoundManager::UpdateSound() {
+    result = system->update();
+}
---
+++ src/Manager/SoundManager.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "fmod.hpp"
+
+class SoundManager {
+public:
+    FMOD::System * system; // Virtual Sound Card
+    FMOD::Sound * sound1, * sound2; // Store Sound File
+    FMOD::Channel * channel = 0; // Buffer
+    FMOD_RESULT result; // HRESULT
+    void * extradriverdata = 0; // Nothing
+
+    void InitializeAudio();
+    void PlaySound1(float pitch, float pan);
+    void PlaySoundTrack();
+    void LoadSounds();
+    void UpdateSound();
+};
