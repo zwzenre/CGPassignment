9384e2e087aa000754d0e21cd5abb1737c227c28
Mingyverse <teoh.yongming456@gmail.com>
2025-09-07 01:59:58 +0800

    sample game class

--- src/Game/Game.cpp
+++ src/Game/Game.cpp
@@ -1,1 +1,29 @@
 #include "Header/Game.h"
+#include <d3d9.h>
+#pragma comment(lib, "d3d9.lib")
+#pragma comment(lib, "d3dx9.lib")
+
+void Game::Initialization() {
+    hr = D3DXCreateSprite(d3dDevice, &spriteBrush);
+    if(FAILED(hr))
+        MessageBox(NULL, "Unable to create sprite brush", "ERROR", MB_ICONSTOP || MB_OK);
+
+
+    hr = D3DXCreateTextureFromFile(d3dDevice, "assets/practical9.png", &texture[0]);
+    if(FAILED(hr))
+        MessageBox(NULL, "Unable to load Texture", "ERROR", MB_ICONSTOP || MB_OK);
+}
+
+void Game::Render() {
+
+}
+
+void Game::Update() {
+
+}
+
+void Game::Quit() {
+
+}
+
+
--- src/Game/Header/Game.h
+++ src/Game/Header/Game.h
@@ -1,8 +1,37 @@
 #include "../../Timer/Header/Timer.h"
+#include "../../Manager/Header/SoundManager.h"
+#include "../../Manager/Header/PhysicsManager.h"
 #include "../../Manager/Header/InputManager.h"
+#include <d3d9.h>
+#include <d3dx9.h>
+#pragma comment(lib, "d3d9.lib")
+#pragma comment(lib, "d3dx9.lib")
+
+#ifndef GAME_GAME_H
+#define GAME_GAME_H
 
 class Game {
 private:
     Timer timer;
+    SoundManager soundManager;
+    PhysicsManager physicsManager;
+    InputManager inputManager;
+    int level;
+    HRESULT hr;
+    LPD3DXSPRITE spriteBrush;
+    LPD3DXFONT fontBrush;
+    LPD3DXLINE lineBrush;
+    D3DPRESENT_PARAMETERS d3dPP;
+    IDirect3DDevice9 *d3dDevice;
+    LPDIRECT3DTEXTURE9 texture[];
+    
 
+
+public:
+    void Initialization();
+    void Render();
+    void Update();
+    void Quit();
 };
+
+#endif //GAME_GAME_H
