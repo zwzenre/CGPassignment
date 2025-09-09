bc7cd8fe40a55db57f7a8b88c2b684b741784702
Mingyverse <teoh.yongming456@gmail.com>
2025-09-09 16:58:52 +0800

    timer conflict fix

--- src/Timer/Header/Timer.h
+++ src/Timer/Header/Timer.h
@@ -1,20 +1,23 @@
 #pragma once
 #include <Windows.h>
+#include <string>
 
-class Timer
-{
+class Timer {
 public:
     void Init(int);
     int FramesToUpdate();
+    std::string GetTimer();
+    void SetTotalTime(int time);
     float GetDeltaTime();
 
 private:
-    LARGE_INTEGER timeFreq;
+    LARGE_INTEGER timerFreq;
     LARGE_INTEGER timeNow;
     LARGE_INTEGER timePrevious;
     int requestedFPS;
     float intervalsPerFrame;
     float intervalsSinceLastUpdate;
 
+    int totalTime = 0;
+//    float interval = 0;
 };
-
--- src/Timer/Timer.cpp
+++ src/Timer/Timer.cpp
@@ -1,33 +1,47 @@
-#include "../Timer/Header/Timer.h"
+#include "Header/Timer.h"
+#include <iostream>
 #include <profileapi.h>
 
-void Timer::Init(int fps)
-{
-    QueryPerformanceFrequency(&timeFreq);
+void Timer::Init(int fps) {
+    QueryPerformanceFrequency(&timerFreq);
     QueryPerformanceCounter(&timeNow);
     QueryPerformanceCounter(&timePrevious);
 
     requestedFPS = fps;
 
-    intervalsPerFrame = (float)(timeFreq.QuadPart / requestedFPS);
-
+    intervalsPerFrame = (float)(timerFreq.QuadPart / requestedFPS);
+    totalTime = 0.0f;
 }
 
-int Timer::FramesToUpdate()
-{
+int Timer::FramesToUpdate() {
     int framesToUpdate = 0;
     QueryPerformanceCounter(&timeNow);
 
-    //get the delta time
-    intervalsSinceLastUpdate = (float)(timeNow.QuadPart - (float)timePrevious.QuadPart);
+    // Get delta time
+    intervalsSinceLastUpdate = (float)timeNow.QuadPart - (float)timePrevious.QuadPart;
 
     framesToUpdate = (int)(intervalsSinceLastUpdate / intervalsPerFrame);
 
-    if(framesToUpdate != 0)
-    {
+    if (framesToUpdate != 0)
         QueryPerformanceCounter(&timePrevious);
-    }
-
 
     return framesToUpdate;
+}
+
+std::string Timer::GetTimer() {
+    int minutes = totalTime / 60;
+    int seconds = totalTime % 60;
+
+//    interval += 0.5;
+//
+//    if ((int)interval % 2 == 0) totalTime++;
+
+    std::string minStr = (minutes < 10) ? "0" + std::to_string(minutes) : std::to_string(minutes);
+    std::string secStr = (seconds < 10) ? "0" + std::to_string(seconds) : std::to_string(seconds);
+
+    return minStr + ":" + secStr;
+}
+
+void Timer::SetTotalTime(int time) {
+    totalTime = time;
 }
